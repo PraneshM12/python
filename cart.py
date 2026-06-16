22573fede70731bdda81d9080e6251ea0896474e
pranesh <praneshm8438@gmail.com>
2026-06-16 23:56:50 +0530

    added

---
+++ cart.py
@@ -0,0 +1,6 @@
+item = input("Enter the item you want to add to the cart: ")
+price =float(input("Enter the price:"))
+quantity = int(input("Enter the quantity: "))
+
+total=price*quantity
+print(total)
---
+++ type.py
@@ -0,0 +1,8 @@
+num= 15
+name ="PranN"
+value= True
+balance=1709.87
+print(type(num))
+print(type(name))
+print(type(value))
+print(type(balance))
