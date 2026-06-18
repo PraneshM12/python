6f58d0112b1df87ab447fe02661a1190f062c939
pranesh <praneshm8438@gmail.com>
2026-06-18 19:43:28 +0530

    added

---
+++ Age.py
@@ -0,0 +1,4 @@
+Name = input("Enter your Name:")
+age = int(input("Enter the Age:"))
+age+=1
+print(f"Happy BirthDay {Name} your are now {age} old")
---
+++ slicing.py
@@ -0,0 +1,8 @@
+Name = "Pranesh"
+print(Name[::1])
+print(Name[:4])
+print(Name[::-1])
+
+web= "https://google.com"
+print(web[slice(8,-4)])
+
