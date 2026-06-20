a58fa2d1cc2d18710f3c5aa2e1d500e12ad47ad1
pranesh <praneshm8438@gmail.com>
2026-06-20 10:28:08 +0530

    pattern program

---
+++ pattern.py
@@ -0,0 +1,12 @@
+sum = 7
+for i in range(1, sum+1):
+    for j in range(i):
+        print(" # " ,end=" ")
+    print()
+
+n=5
+for i in range(5):
+    for j in range(i):
+        print(i, end=" ")
+
+    print()
