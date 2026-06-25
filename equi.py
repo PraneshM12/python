dffeb0656419907aa991dd3ee8461a63f4147ecf
pranesh <praneshm8438@gmail.com>
2026-06-25 19:57:17 +0530

    new program

---
+++ equi.py
@@ -0,0 +1,20 @@
+def equilibrium_index(arr):
+
+    total_sum = sum(arr)
+    left_sum = 0
+
+    for i in range(len(arr)):
+
+        total_sum -= arr[i]  # right sum
+
+        if left_sum == total_sum:
+            return i
+
+        left_sum += arr[i]
+
+    return -1
+
+
+arr = [1, 3, 5, 2, 2]
+
+print("Equilibrium Index:", equilibrium_index(arr))
